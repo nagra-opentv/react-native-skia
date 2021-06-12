d529c8bab7b32c5410c3dce7a67ce230dae8d8b8
pprabhu-ng <pprabhu.wns@gmail.com>
2021-06-12 23:50:32 +0530

    Resolve conflicts and update changes from review comments

--- ReactSkia/components/RSkComponentText.h
+++ ReactSkia/components/RSkComponentText.h
@@ -22,7 +22,7 @@ class RSkComponentRawText final : public RSkComponent {
   void OnPaint(SkCanvas *canvas) override;
 };
 
-class RSkComponentParagraph final : public RSkComponent ,public RSkTextLayoutManager{
+class RSkComponentParagraph final : public RSkComponent {
  public:
   RSkComponentParagraph(const ShadowView &shadowView);
   std::shared_ptr<skia::textlayout::ParagraphBuilder> paraBuilder;
@@ -57,6 +57,8 @@ class RSkComponentParagraph final : public RSkComponent ,public RSkTextLayoutMan
      }
      return nullptr;
   }
+
+  RSkTextLayoutManager textLayoutManager_;
 
 };
 
