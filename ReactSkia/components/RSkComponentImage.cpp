243b8e752a4947c9705f1b812affb063b2adf5ce
Shefaya <shefayaparwin@gmail.com>
2021-07-21 16:52:59 +0530

    RNS implementation for Image Resizemode props

--- ReactSkia/components/RSkComponentImage.cpp
+++ ReactSkia/components/RSkComponentImage.cpp
@@ -1,9 +1,12 @@
-#include "ReactSkia/components/RSkComponentImage.h"
 #include "include/core/SkPaint.h"
-#include "ReactSkia/views/common/RSkDrawUtils.h"
-#include "ReactSkia/views/common/RSkImageCacheManager.h"
-#include "react/renderer/components/image/ImageShadowNode.h"
+#include "include/core/SkClipOp.h"
+#include "include/core/SkImageFilter.h"
+#include "include/effects/SkImageFilters.h"
 
+#include "ReactSkia/components/RSkComponentImage.h"
+#include "ReactSkia/views/common/RSkDrawUtils.h"
+#include "ReactSkia/views/common/RSkImageUtils.h"
+#include "ReactSkia/views/common/RSkImageCacheManager.h"
 #include "ReactSkia/utils/RnsLog.h"
 #include "ReactSkia/utils/RnsUtils.h"
 
@@ -11,6 +14,7 @@ namespace facebook {
 namespace react {
 
 using namespace RSkDrawUtils;
+using namespace RSkImageUtils;
 using namespace RSkImageCacheManager;
 
 RSkComponentImage::RSkComponentImage(const ShadowView &shadowView)
@@ -22,6 +26,7 @@ void RSkComponentImage::OnPaint(
   auto const &imageProps =
       *std::static_pointer_cast<ImageProps const>(component.props);
   if (imageProps.sources.empty()) {
+    RNS_LOG_ERROR("Empty Source ....");
     return;
   }
   const auto source = imageProps.sources[0];
@@ -31,7 +36,7 @@ void RSkComponentImage::OnPaint(
     std::string path = "./" + source.uri.substr(7);
 
     Rect frame = getAbsoluteFrame();
-    SkRect rect = SkRect::MakeXYWH(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
+    SkRect frameRect = SkRect::MakeXYWH(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
     auto const &imageBorderMetrics=imageProps.resolveBorderMetrics(component.layoutMetrics);
 	
     /* Draw order 1. Background 2. Image 3. Border*/
@@ -39,7 +44,19 @@ void RSkComponentImage::OnPaint(
     RNS_PROFILE_START(drawImage)
     sk_sp<SkImage> imageData=getImageData(path.c_str());
     if(imageData) {
-      canvas->drawImageRect(imageData, rect, nullptr);
+      canvas->save();
+      SkRect targetRect = computeTargetRect({imageData->imageInfo().width(),imageData->imageInfo().height()},frameRect,imageProps.resizeMode);
+      /* clipping logic to be applied if computed Frame is greater than the target.*/
+      if(( frameRect.width() < targetRect.width()) || ( frameRect.height() < targetRect.height())) {
+        canvas->clipRect(frameRect,SkClipOp::kIntersect);
+      }
+      SkPaint paint;
+      if(imageProps.resizeMode == ImageResizeMode::Repeat){
+        sk_sp<SkImageFilter> imageFilter(SkImageFilters::Tile(targetRect,frameRect ,nullptr));
+        paint.setImageFilter(std::move(imageFilter));
+      }
+      canvas->drawImageRect(imageData,targetRect,&paint);
+      canvas->restore();
     } else {
       RNS_LOG_ERROR("Draw Image Failed for:" << path);
     }
